5d34eff1e377c1629d3c1ca5a854edd391dba22c
Clay <rholt16@uncc.edu>
2021-03-12 21:58:45 -0500

    new animation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
 const canvas = document.getElementById("canvas");
+canvas.width = canvas.width *  2;
+ canvas.height = canvas.height * 2;
+
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true, precision: "mediump"});
 
 camera.position.z = 600;
@@ -30,32 +33,26 @@ let yNoise = 0;// (Math.random() - 0.5) * 5;
 let dx = randomNumber(100, 300) / 1000;
 let dy = randomNumber(100, 300) / 1000;
 let MAX_DRIFT = 6;
+let driftX = 1;
 
-console.log(dx, dy);
-
+let x, y;
 function nextPoint(){
-    // let xNoise = (Math.random() - 0.5) * 10;
-    // let yNoise = (Math.random() - 0.5) * 10;
 
-    if(xNoise > MAX_DRIFT || xNoise < - MAX_DRIFT){
+    if(xNoise >= MAX_DRIFT || xNoise <= - MAX_DRIFT){
         dx = - dx// (randomNumber(10, 30) / 100)
-        // console.log("x shift")
     }
 
-    if(yNoise > MAX_DRIFT || yNoise < - MAX_DRIFT){
+    if(yNoise >= MAX_DRIFT || yNoise <= - MAX_DRIFT){
         dy = - dy //(randomNumber(10, 30) / 100)
-        // console.log("y shift")
     }
-    // console.log("here", dx, dy);
     xNoise += dx;
     yNoise += dy;
-    // let timer = Date.now() * 0.00012;
-    // xNoise = Math.sin(timer) * dx;
-    // console.log(xNoise)
 
-    return new THREE.Vector3(Math.sin(counter * (Math.PI / 180)) * (counter * .1) + xNoise,
-        Math.cos(counter * (Math.PI / 180)) * (counter * .1) + yNoise,
-        0) ;
+    x = Math.sin(counter * (Math.PI / 180)) * (counter * .1) + xNoise
+    y = Math.cos(counter * (Math.PI / 180)) * (counter * .1) + yNoise
+
+    return new THREE.Vector3(x, y,
+        0);
 }
 
 function nextColor(){
@@ -71,15 +68,16 @@ function nextColor(){
 
 
 }
+let interval = setInterval(animate, 15);
 
 function animate() {
     counter++;
     let p = nextPoint();
     points.push(p);
 
-    geometry = new THREE.BufferGeometry().setFromPoints(points);
-    material = new THREE.LineBasicMaterial({color: color});
-    line = new THREE.Line(geometry, material);
+    let geometry = new THREE.BufferGeometry().setFromPoints(points);
+    let material = new THREE.LineBasicMaterial({color: color});
+    let line = new THREE.Line(geometry, material);
     // scene.add(new THREE.Line(new THREE.BufferGeometry().setFromPoints(points), new THREE.LineBasicMaterial({color: color})));
     scene.add(line);
     renderer.render(scene, camera);
@@ -87,11 +85,28 @@ function animate() {
     if(counter % 120 === 0){
         points = points.slice(points.length - 1);
         nextColor();
+        MAX_DRIFT += driftX
+        dx *= (1 + .1 * Math.random());
+        dy *= (1 + .1 * Math.random());
+
+        if(MAX_DRIFT % 40 === 0){
+            driftX = driftX * 2;
+        }
+
+        // document.getElementById("x").innerText = x;
+        // document.getElementById("y").innerText = y;
+        // document.getElementById("dx").innerText = dx;
+        // document.getElementById("dy").innerText = dy;
+        // document.getElementById("driftx").innerText = driftX;
+        // document.getElementById("max-drift").innerText = MAX_DRIFT;
     }
-    // requestAnimationFrame( animate );
+
+    if(Math.abs(x) > 2000 || Math.abs(y) > 2000){
+        clearInterval(interval);
+        console.log("STOPPED");
+    }
 }
 
-setInterval(animate, 15);
 
 function randomNumber(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min+1)+min);
